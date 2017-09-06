752dbcb4fb285b7935524e26e1ce7fa3d9bfdd09
Martinusbe <martinusbe@gmail.com>
2017-09-06 22:08:36 +0200

    op3: gzosp

--- gzosp.mk
+++ gzosp.mk
@@ -20,8 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common gzosp stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_z2_plus
 PRODUCT_DEVICE := z2_plus
