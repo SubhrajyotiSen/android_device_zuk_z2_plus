47ec0436469707e7284951e6feeda9eab222c2d8
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-09-06 23:36:37 +0530

    It's lineage
    Change-Id: Ic18672daf583eb4a6554f780b0891108520ea540

--- lineage.mk
+++ lineage.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
 # Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_z2_plus
 PRODUCT_DEVICE := z2_plus
