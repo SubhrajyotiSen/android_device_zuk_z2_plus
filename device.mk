d2d47e3054a3ee62a1c2f7f30760cefbdd6bb83b
Martinusbe <martinusbe@gmail.com>
2017-09-06 23:10:37 +0200

    op3: create overlay-gzosp
     * gzosp specific overlays will be here
     * removed cmsdk overlays

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product-if-exists, vendor/zuk/z2_plus/z2_plus-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
-#    $(LOCAL_PATH)/overlay-lineage
+#    $(LOCAL_PATH)/overlay-gzosp
 
 # Permissions
 PRODUCT_COPY_FILES += \
