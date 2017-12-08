9d3eec45a2c8516b5cecfd3464d6bdbeaa021bda
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-12-08 20:38:19 +0000

    Revert "msm8996-common: init: Use core init function to read from files"
    This reverts commit a630dd901b15a1ce91588c18a5de4acc7a2d8b48.

--- init/Android.mk
+++ init/Android.mk
@@ -19,9 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := \
-    system/core/base/include \
-    system/core/init
+LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_z2_plus.cpp
 LOCAL_MODULE := libinit_z2_plus
